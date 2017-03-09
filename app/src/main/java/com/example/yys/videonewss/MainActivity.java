50cd9a8d2ba3f2e9054fc11aeefe131a9d2cb705
yysshan <2177785186@qq.com>
2017-03-09 23:04:46 +0800

    第一次修改提交

--- app/src/main/java/com/example/yys/videonewss/MainActivity.java
+++ app/src/main/java/com/example/yys/videonewss/MainActivity.java
@@ -7,38 +7,17 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.BindViews;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.bt1)Button button;
-    @BindView(R.id.bt2)Button button1;
 
-    private Unbinder unbinder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        unbinder =  ButterKnife.bind(this);
 
-
-
-    }
-    @OnClick({R.id.bt1,R.id.bt2})
-    public void onclick(View view){
-        switch (view.getId()){
-            case R.id.bt1:
-                Toast.makeText(this, "ssss", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.bt2:
-                Toast.makeText(this, "yyyssss", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unbinder.unbind();
     }
 }
